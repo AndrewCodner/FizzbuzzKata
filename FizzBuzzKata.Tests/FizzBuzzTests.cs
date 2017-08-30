5fad5b5a5ad67fdf0ef5cd641216975e0afa60c1
chilli <chilli@chilli.local>
2017-08-30 09:38:02 +0200

    Completed two test cases

---
+++ FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -0,0 +1,41 @@
+﻿using FizzbuzzKata;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+       [Test]
+       public void FizzbuzzChecker_GivenIntegeroNumberOne_ShouldReturnStringNumberOne ()
+        {
+            //Arrange
+            int input = 1;
+            string expected = "1";
+            //Act
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            var result = fizzbuzz.FizzbuzzChecker(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void FizzbuzzChecker_GivenIntegeroNumberTwo_ShouldReturnStringNumberTwo()
+        {
+            //Arrange
+            int input = 2;
+            string expected = "2";
+            //Act
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            var result = fizzbuzz.FizzbuzzChecker(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
---
+++ FizzbuzzKata/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzKata
+{
+    public class FizzBuzz
+    {
+        public string FizzbuzzChecker(int input)
+        {
+            if(input==1)
+            {
+                return "1";
+            }
+            else 
+            {
+                return "2";
+            }            
+        }
+    }
+}
